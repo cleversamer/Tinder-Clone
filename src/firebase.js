5bfe724360196a99de1c0653e877d30b208f746e
Samer A <thedev.samer@gmail.com>
2022-05-27 11:15:53 +0300

    added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const config = {
+  apiKey: process.env["REACT_APP_FIREBASE_API_KEY"],
+  authDomain: "samer-tinder.firebaseapp.com",
+  projectId: "samer-tinder",
+  storageBucket: "samer-tinder.appspot.com",
+  messagingSenderId: "687341526934",
+  appId: "1:687341526934:web:145382fc7a81d30e9547b7",
+  measurementId: "G-7N7PJZK6Y5",
+};
